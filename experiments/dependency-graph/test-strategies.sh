a189c32f446ee64ab908c6340ec4eef540dfceb4
Olivier Cots <olivier.cots@irit.fr>
2026-06-21 14:38:36 +0200

    remove all - see v1 tag if needed

--- experiments/dependency-graph/test-strategies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Test different strategies to retrieve dependency graph with versions
-
-echo "=== Strategy 1: Julia Pkg.status() approach ==="
-echo "Creating empty environment and installing OptimalControl..."
-
-# This will show us what we get from Pkg.status
